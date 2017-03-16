e90a111a24b3353dfe51abe7681d7b7523bbc327
Nicklas Nyegaard <nnyegaard@gmail.com>
2017-03-16 15:51:27 +0100

    Init commit

---
+++ src/HashGeneration/GenerateHash.cs
@@ -0,0 +1,26 @@
+﻿namespace HashGeneration
+{
+    using System;
+    using System.Security.Cryptography;
+    using System.Text;
+
+    public class HashGeneration
+    {
+        public static string Generate(HashAlgorithm algorithm = null)
+        {
+            if (algorithm == null)
+            {
+                algorithm = SHA512.Create();
+            }
+
+            using (algorithm)
+            {
+                string hash = $"{Guid.NewGuid()} {DateTime.UtcNow}";
+                byte[] byteHash = algorithm.ComputeHash(Encoding.UTF8.GetBytes(hash));
+                string computedHash = Convert.ToBase64String(byteHash);
+
+                return computedHash;
+            }
+        }
+    }
+}
